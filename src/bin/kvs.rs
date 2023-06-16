eaf2da46faea9d53479a3491e0479e5933a822d8
mx <max@maxvl.com>
2023-06-16 09:34:01 -0400

    Start adding basic parser functionality

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    key: String
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, {:?}!", args.key);
 }
 
 
