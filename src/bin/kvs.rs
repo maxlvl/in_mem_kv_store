421d34bb6bcccf67469b6e84c16cba0818c92d86
mx <max@maxvl.com>
2023-06-16 22:33:33 -0400

    Run clippy & fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,60 +1,44 @@
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 use std::process::exit;
-
 
 fn main() {
     let cmd = Command::new("kvs")
-    .author(env!("CARGO_PKG_AUTHORS"))
-    .version(env!("CARGO_PKG_VERSION"))
-    .about(env!("CARGO_PKG_DESCRIPTION"))
-    .after_help("Please provide one of the following: --key")
-    .subcommand(
-        Command::new("set")
-        .about("Set a key-value pair")
-        .arg(
-            Arg::new("key")
-            .required(true)
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .after_help("Please provide one of the following: --key")
+        .subcommand(
+            Command::new("set")
+                .about("Set a key-value pair")
+                .arg(Arg::new("key").required(true))
+                .arg(Arg::new("value").required(true)),
         )
-        .arg(
-            Arg::new("value")
-            .required(true)
+        .subcommand(
+            Command::new("get")
+                .about("Get a value by key")
+                .arg(Arg::new("key").required(true)),
         )
-    )
-    .subcommand(
-        Command::new("get")
-        .about("Get a value by key")
-        .arg(
-            Arg::new("key")
-            .required(true)
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a key-value pair")
+                .arg(Arg::new("key").required(true)),
         )
-    )
-    .subcommand(
-        Command::new("rm")
-        .about("Remove a key-value pair")
-        .arg(
-            Arg::new("key")
-            .required(true)
-        )
-    )
-    .get_matches();
+        .get_matches();
 
     match cmd.subcommand_name() {
         Some("set") => {
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         Some("get") => {
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         Some("rm") => {
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         None => panic!("No subcommand provided"),
         _ => panic!("Invalid subcommand provided"),
     }
-
 }
-
-
